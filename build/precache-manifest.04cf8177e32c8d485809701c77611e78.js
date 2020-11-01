d6732a525872c2743eb46185e54f1e1346b7bdd4
Your Name <helenemartin13@hotmail.com>
2020-11-01 09:14:53 +0000

    first commit

---
+++ build/precache-manifest.04cf8177e32c8d485809701c77611e78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9b8edb176455f3ba825c44429e5a01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "511526be46eb23a2985c",
+    "url": "/static/css/main.6942973f.chunk.css"
+  },
+  {
+    "revision": "73a2a348ec369e69a532",
+    "url": "/static/js/2.fa8233f7.chunk.js"
+  },
+  {
+    "revision": "511526be46eb23a2985c",
+    "url": "/static/js/main.23d53a60.chunk.js"
+  },
+  {
+    "revision": "49155c0d22ee4ca7cc89",
+    "url": "/static/js/runtime-main.0bca4086.js"
+  }
+]);
